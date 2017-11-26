250c070dc3d1579d6668012e60c1c749dff08bd1
Michal Zaborowski <michal@zaborowski.info.pl>
2017-11-26 18:08:46 +0100

    cleaning, sample model that works

--- social-model/model.py
+++ social-model/model.py
@@ -5,13 +5,44 @@ from random import random, uniform
 
 class GlobalState:
     """Global state definition, as a model of population."""
+    logger = logging.getLogger('GlobalState')
     _nodes = []
+    _edges = []
 
-    def nodes(self):
+    def getNodes(self):
         return self._nodes
 
     def neighbors(self, node):
-        return []
+        res = []
+        for e in self._edges:
+            if not node in e:
+                continue
+            node = list(e - {node})[0]
+            res.append(node)
+        return res
+
+    def addEdge(self, node1, node2):
+        if node1 == node2:
+            self.logger.debug('same node, at both ends ' + str(node1.id))
+            return
+        edge = {node1, node2}
+        nodes = list(edge)
+        if (edge in self._edges):
+            self.logger.debug('edge already present (%d<->%d)', nodes[0].id, nodes[1].id)
+            return
+        self._edges.append(edge)
+
+    def removeEdge(self, node1, node2):
+        edge = {node1, node2}
+        nodes = list(edge)
+        if (edge in self._edges):
+            self.logger.debug('removing edge (%d<->%d)', nodes[0].id, nodes[1].id)
+            self._edges.remove(edge)
+        else:
+            self.logger.debug('there was no that edge (%d<->%d)', nodes[0].id, nodes[1].id)
+
+    def getEdges(self):
+        return self._edges
 
 
 class Citizen:
@@ -20,46 +51,45 @@ opinion - like party choice
 action - which determines if change of option is more comfortable
 neighbors - as a edges in graph connected to this node
     """
-    def __init__(self, opinion, action, pos=(0, 0)):
+    def __init__(self, id, opinion, action, pos=(0, 0)):
+        self.id = id
         self.opinion = opinion
         self.action = action
         self.pos = pos
+        self._neighbors = []
 
     def __str__(self):
-        return (str(self.opinion), str(self.action))
-
-    def neighbors(self, globalState):
-        return globalState.neighbors(self)
+        return (str(self.id), str(self.opinion), str(self.action))
 
 
 def socialRemake(globalState, node, referenceLevel):
+    logger = logging.getLogger('dif_ind_neigh')
     neig = globalState.neighbors(node)
-    notNeigh = [n for n in globalState.nodes() if n not in neig]
+    notNeigh = [n for n in globalState.getNodes() if n not in neig]
 
-    rand = uniform(1, 6)
+    rand = uniform(0, 1)
     if rand < referenceLevel:
         lmnotneigh = [m for m in notNeigh if node.opinion - m.action <= 0.5 and node.opinion - m.action >= -0.5]
         for d in lmnotneigh:
-            globalState.add_edge(node, d)
-            print("edge added")
+            logger.debug("adding edge for social remake")
+            globalState.addEdge(node, d)
             break
     if rand > referenceLevel:
         diffneig = [l for l in neig if node.opinion - l.opinion > 0.5 or node.opinion - l.opinion < -0.5]
         for f in diffneig:
-            globalState.remove_edge(node, f)
-            print("edge removed")
+            logger.debug("removing edge for social remake")
+            globalState.removeEdge(node, f)
             break
 
 
 def differeciateIndividualNeighbor(globalState, node, referenceLevel):
     logger = logging.getLogger('dif_ind_neigh')
     rand = uniform(0, 1)
-    logger.debug(" dice " + str(rand))
     if rand > referenceLevel:
         logger.debug("skiping")
         return
     # list of neighbors
-    neighbors = node.neighbors(globalState)
+    neighbors = globalState.neighbors(node)
     # checking agains div by zero
     if len(neighbors) == 0:
         logger.debug("no neighbors - skipping")
@@ -74,17 +104,17 @@ def differeciateIndividualNeighbor(globalState, node, referenceLevel):
     if diff <= -1:
         prev = node.action
         node.action += 1
-        logger.debug("action changed - from " + str(prev) + " to " + str(node.action))
+        logger.debug("action changed - from %d to %d ", prev, node.action)
     elif diff >= 1:
         prev = node.action
         node.action = max(1, node.action - 1)
-        logger.debug("action changed - from " + str(prev) + " to " + str(node.action))
+        logger.debug("action changed - from %d to %d ", prev, node.action)
 
 
-def prepare_random_citizen():
+def prepare_random_citizen(idx):
     randOpinion = uniform(0.5, 3.5)
     randAction = int(random() * 100) % 3 + 1
-    return Citizen(randOpinion, randAction, (random(), random()))
+    return Citizen(idx, randOpinion, randAction, (random(), random()))
 
 
 def main():
@@ -94,13 +124,22 @@ def main():
     logger.info('so - starting')
 
     globalState = GlobalState()
-    logger.info('generating')
+    nodes = globalState.getNodes()
+    logger.info('generating nodes')
     for idx in range(100):
-        globalState.nodes().append(prepare_random_citizen())
+        nodes.append(prepare_random_citizen(idx))
+    logger.info('generating edges')
+    for node in globalState.getNodes():
+        for idx in range(10):
+            otherEnd = nodes[int(uniform(0,len(nodes)))]
+            globalState.addEdge(node, otherEnd)
 
     logger.info('processing')
     for timestep in range(100):
-        troubled = [t for t in globalState.nodes() if t.opinion - t.action >= 0.5 or t.opinion - t.action <= -0.5]
+        logger.info('iteration - %d', timestep)
+        troubled = [t for t in globalState.getNodes() if t.opinion - t.action >= 0.5 or t.opinion - t.action <= -0.5]
+        logger.info("nodes - %d, edges - %d, troubled - %d",
+                    len(globalState.getNodes()), len(globalState.getEdges()), len(troubled))
         for node in troubled:
             socialRemake(globalState, node, 0.7)
             differeciateIndividualNeighbor(globalState, node, 0.5)
